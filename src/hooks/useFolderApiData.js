b0ee8ddb58a5bc1297c52fb72a06b82e13436f58
severikupari1 <severikupari1@gmail.com>
2023-03-21 17:46:55 +0200

    [AVAPALJU-113] Mocked hook to fix errors when using axios retry

--- src/hooks/useFolderApiData.js
+++ src/hooks/useFolderApiData.js
@@ -1,6 +1,6 @@
 import { getIdToken } from "../service/AuthService";
 import { config } from "../App";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { axiosAuth } from "../service/AxiosAuth";
 import { HttpStatusCode } from "axios";
 
@@ -8,28 +8,26 @@ export const useFolderApiData = (folder, t) => {
   const [ idToken ] = useState(getIdToken())
   const [ rowData, setRowData ] = useState([]);
   const [ fetchError, setFetchError ] = useState(false);
-
   useEffect(() => {
-
     axiosAuth.get(`${ config.apiUrl }${ folder || config.defaultFolder }/`,)
-      .then(res => {
-        if (res.status !== HttpStatusCode.Ok) {
-          throw Error(res.statusText);
-        }
-        return res.data;
-      })
-      .then(jsonRes => {
-        if (jsonRes.aineisto) {
-          setRowData(jsonRes.aineisto);
-        } else {
-          throw Error('Malformed response');
-        }
-      })
-      .catch(error => {
-        console.log(error);
-        setFetchError(true);
-        setRowData([]);
-      });
+        .then(res => {
+          if (res.status !== HttpStatusCode.Ok) {
+            throw Error(res.statusText);
+          }
+          return res.data;
+        })
+        .then(jsonRes => {
+          if (jsonRes.aineisto) {
+            setRowData(jsonRes.aineisto);
+          } else {
+            throw Error('Malformed response');
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          setFetchError(true);
+          setRowData([]);
+        });
   }, [ folder, t, idToken ]);
 
   return {
